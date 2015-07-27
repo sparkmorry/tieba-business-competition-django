aefdac20c65eb63f291e2fce0bdc10fdc489e8f4
sparkmorry <morryxia@gmail.com>
2015-07-27 13:09:03 +0800

    修改邀请函封面及陀螺仪速度

--- static/js/invitation/main.js
+++ static/js/invitation/main.js
@@ -108,7 +108,7 @@ var imageLoader = new LoadImages();
 	 *
 	 */
 	var onMotion = function(e) {
-		// console.log(vec3);
+		console.log(vec3);
 		vec3.set(e.vec3.x, e.vec3.y, e.vec3.z);
 		if (range) vec3 = blocker.block(vec3);
 		rotate(vec3);
@@ -116,11 +116,13 @@ var imageLoader = new LoadImages();
 	var box = $('.t-ios-main .box');
 	var track = $('.t-track .track-inner');
 	// var rotateStr = 'translate3d(0, 0, 500px) rotateZ({2}deg) rotateX(-90deg) rotateX({0}deg) rotateY({1}deg) rotateY(0deg)';
-	var rotateStr = 'translate3d({3}px, 0, 500px) rotateZ({2}deg) rotateX({0}deg) rotateY({1}deg)';
+	var rotateStr = 'translate3d({3}px, 0, 500px) rotateZ({2}deg) rotateX(0deg) rotateX({0}deg) rotateY({1}deg)';
 	var trackStr = 'rotateX({0}deg) rotate({1}deg)';
-	var rotate = function(e){
-		var rotateEnd = rotateStr.replace('{1}', 0).replace('{0}', 0).replace('{2}', (e.z % 360000) * -1).replace('{3}', (e.x-60) * -1);
+	var rotate = function(e){	
+		var rotateY = e.x * -1;
+		var rotateEnd = rotateStr.replace('{1}', 0).replace('{0}', 0).replace('{2}', (e.z % 360000) * -1).replace('{3}', (e.x-40) * -1.4);
 		// var rotateEnd = rotateStr.replace('{1}', e.x * -1).replace('{0}', 0).replace('{2}', (e.z % 3600000) * -1);
+		// var rotateEnd = rotateStr.replace('{1}', e.x * -1).replace('{0}', e.y).replace('{2}', (e.z % 3600000) * -1);
 		var trackEnd = trackStr.replace('{0}', e.y - 30).replace('{1}', -1*e.x%360);
 		box[0].style.webkitTransform = rotateEnd;
 		track[0].style.webkitTransform = trackEnd;
@@ -163,7 +165,7 @@ var imageLoader = new LoadImages();
 		user.addEventListener('supportStatusUpdate', onSupportUpdate);
 
 		//图片加载完成之后，执行设备测试:1
-		coverFade();
+		// coverFade();
 		slideInit();
 		user.test();
 
